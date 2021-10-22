ca08d9babe9f0ad21d9c16bd39767ff7dab129eb
Zay Min Maw <zayminmaw77@gmail.com>
2021-10-23 00:05:30 +0630

    make sured username doesn't duplicate

--- db/repository/user.py
+++ db/repository/user.py
@@ -1,8 +1,12 @@
 from db.models.user import User
 from core.hashing import Hash
 from sqlalchemy.orm import Session
+from fastapi import HTTPException,status
 
 def create(request:User,db:Session):
+    user = db.query(User).filter(User.username == request.username).first()
+    if user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Username already exist.")
     new_user = User(username=request.username,password=Hash.hash_password(request.password),role=request.role)
     db.add(new_user)
     db.commit()
