dd5f8b4c2ab8987fd4d4d7d1217a958c6b953625
Zay Min Maw <zayminmaw77@gmail.com>
2021-10-23 12:23:10 +0630

    runable with python -m main

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from db.base import Base
 from db.session import engine
 from core.config import settings 
 from apis.base import router
+import uvicorn
 
 def include_router(app):
     app.include_router(router)
 
 def create_tables():          
-    print("CREATE TABLE")
     Base.metadata.create_all(bind=engine)
 
 def start_application():
@@ -18,3 +18,6 @@ def start_application():
 	return app
 
 app = start_application()
+
+if __name__=='__main__':
+	uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True, log_level='info')
