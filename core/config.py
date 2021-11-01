db757e510c8023e7496d0cb958e7352229f017d6
thuaung30 <thuhtetaung.d2@gmail.com>
2021-11-01 20:13:45 +0630

    set up ci/cd with github actions and heroku

--- core/config.py
+++ core/config.py
@@ -12,15 +12,17 @@ class Settings:
 
     #DB
     #if your username password contains special characters, you will have to use URI escapes.
-    POSTGRES_USER : str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD") 
-    POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
-    POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432)
-    POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
-    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    # POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+    # POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD") 
+    # POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
+    # POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432)
+    # POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
+    # DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    DATABASE_URL = os.getenv("DATABASE_URL") # heroku postgres comes with one line database url
     
     #This is for zmm's problematic password -_-
-    DATABASE_URL_ALEMBIC =  f"postgresql://{POSTGRES_USER}:%{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    # DATABASE_URL_ALEMBIC =  f"postgresql://{POSTGRES_USER}:%{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    DATABASE_URL_ALEMBIC = os.getenv("DATABASE_URL") # heroku postgres comes with one line database url
 
     #JWT
     SECRET_KEY : str = os.getenv("SECRET_KEY")
