3d2a0fe929164f1d2fc2d5944e7becbc59181347
Zay Min Maw <zayminmaw77@gmail.com>
2021-12-02 23:16:12 +0630

    changed current_user type hint

--- apis/routes/patient.py
+++ apis/routes/patient.py
@@ -6,32 +6,32 @@ from db.repository import patient
 from db.session import get_db
 from typing import List
 from services.oauth2 import get_current_user
-from schemas.user import User
+from schemas.token import TokenData
 
 router = APIRouter(prefix="/patients", tags=["Patients"])
 
 @router.get('/',status_code=status.HTTP_200_OK, response_model=List[showPatient])
-def get_all_patients(db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+def get_all_patients(db: Session = Depends(get_db),current_user: TokenData = Depends(get_current_user)):
     return patient.readAll(db)
 
 
 @router.get('/{id}',status_code=status.HTTP_200_OK,response_model=showPatient)
-def get_patient(id: int, db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+def get_patient(id: int, db: Session = Depends(get_db),current_user: TokenData = Depends(get_current_user)):
     return patient.read(id,db)
 
 
 @router.post("/", status_code=status.HTTP_200_OK, response_model=Message)
-def create(request: Patient, db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+def create(request: Patient, db: Session = Depends(get_db),current_user: TokenData = Depends(get_current_user)):
     patient.create(request, db,current_user)
     return {"detail": "Patient create successful."}
 
 @router.put("/{id}",status_code=status.HTTP_200_OK,response_model=Message)
-def update(id: int, request: Patient,db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+def update(id: int, request: Patient,db: Session = Depends(get_db),current_user: TokenData = Depends(get_current_user)):
     patient.update(id,request,db,current_user)
     return {"detail": "Patient update successful."}
 
 
 @router.delete("/{id}",status_code=status.HTTP_200_OK, response_model=Message)
-def delete(id: int, db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+def delete(id: int, db: Session = Depends(get_db),current_user: TokenData = Depends(get_current_user)):
     patient.delete(id,db)
     return {"detail": "Petient delete successful."}
--- apis/routes/uom.py
+++ apis/routes/uom.py
@@ -6,29 +6,29 @@ from db.repository import uom
 from db.session import get_db
 from typing import List
 from services.oauth2 import get_current_user
-from schemas.user import User
+from schemas.token import TokenData
 
 router = APIRouter(prefix="/uom", tags=["UOM"])
 
 @router.get('/',status_code=status.HTTP_200_OK, response_model=List[showUom])
-def get_all_uom(db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+def get_all_uom(db: Session = Depends(get_db),current_user: TokenData = Depends(get_current_user)):
     return uom.readAll(db)
 
 @router.get('/{id}',status_code=status.HTTP_200_OK,response_model=showUom)
-def get_uom(id: int, db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+def get_uom(id: int, db: Session = Depends(get_db),current_user: TokenData = Depends(get_current_user)):
     return uom.read(id,db)
 
 @router.post("/", status_code=status.HTTP_200_OK, response_model=Message)
-def create(request: Uom, db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+def create(request: Uom, db: Session = Depends(get_db),current_user: TokenData = Depends(get_current_user)):
     uom.create(request, db,current_user)
     return {"detail": "UOM create successful."}
 
 @router.put("/{id}",status_code=status.HTTP_200_OK,response_model=Message)
-def update(id: int, request: Uom,db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+def update(id: int, request: Uom,db: Session = Depends(get_db),current_user: TokenData = Depends(get_current_user)):
     uom.update(id,request,db,current_user)
     return {"detail": "UOM update successful."}
 
 @router.delete("/{id}",status_code=status.HTTP_200_OK, response_model=Message)
-def delete(id: int, db: Session = Depends(get_db),current_user: User = Depends(get_current_user)):
+def delete(id: int, db: Session = Depends(get_db),current_user: TokenData = Depends(get_current_user)):
     uom.delete(id,db)
     return {"detail": "UOM delete successful."}
