52980274ea63d8b6823e9f644f1bcc1a05434581
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-06-19 16:39:26 +0630

    fix: requested changes

--- core/services/inventory.py
+++ core/services/inventory.py
@@ -57,21 +57,7 @@ class InventoryService:
     def createMultipleInventoryItem(self,inventoryItems) -> None:
         try:
             for inventoryItem in inventoryItems:
-                if type(inventoryItem) is not dict:
-                    inventoryItem = inventoryItem.dict()
-                initial_balance = inventoryItem["balance"]
-                inventoryItem["balance"] = 0
-                new_inventoryItem = self.inventory_repo.persistInventoryItem(inventoryItem)
-                if initial_balance > 0:
-                    self.createInventoryTransaction\
-                        ({"inventory_item_id":new_inventoryItem.id,\
-                        "inventory_item_name":new_inventoryItem.name,\
-                        "transaction_type_name":"Adjustment In",\
-                        "transaction_type":type_enum.receive,\
-                        "quantity":initial_balance,\
-                        "unit":new_inventoryItem.unit,\
-                        "purchasing_price":new_inventoryItem.purchasing_price,\
-                        "selling_price":new_inventoryItem.sales_service_item.price})
+                self.createInventoryItem(inventoryItem)
         except HTTPException as e:
             raise BAD_REQUEST(e.detail)
         return
@@ -142,22 +128,7 @@ class InventoryService:
     def createMultipleInventoryTransaction(self,inventoryTransactions) -> None:
         try:
             for inventoryTransaction in inventoryTransactions:
-                if type(inventoryTransaction) is not dict:
-                    inventoryTransaction = inventoryTransaction.dict()
-                inventoryItem = self.inventory_repo.getInventoryItemById(inventoryTransaction["inventory_item_id"])
-                total = inventoryItem.balance
-                if inventoryTransaction["transaction_type"] == type_enum.receive:
-                    total = inventoryItem.balance + inventoryTransaction["quantity"]
-                    self.inventory_repo.updateInventoryItem\
-                        (inventoryItem.id,{"balance":total})
-                elif inventoryTransaction["transaction_type"] == type_enum.issue:
-                    if inventoryTransaction["quantity"] > inventoryItem.balance:
-                        raise BAD_REQUEST("Not enough "+ inventoryTransaction["inventory_item_name"] +" in inventory.")
-                    total = inventoryItem.balance - inventoryTransaction["quantity"]
-                    self.inventory_repo.updateInventoryItem\
-                        (inventoryItem.id,{"balance":total})
-                self.inventory_repo.persistInventoryTransaction(dict(inventoryTransaction,opening_balance=inventoryItem.balance,closing_balance=total))
-                return 
+                self.createInventoryTransaction(inventoryTransaction)
         except HTTPException as e:
             raise BAD_REQUEST(e.detail)
         return
@@ -174,13 +145,7 @@ class InventoryService:
     def createMultiplePharmacyItem(self,pharmacyItems) -> None:
         try:
             for pharmacyItem in pharmacyItems:
-                pharmacyItem = pharmacyItem.dict()
-                inventoryItem = pharmacyItem["with_inventory"]
-                pharmacyItem.pop("with_inventory")
-                new_pharmacyItem = self.inventory_repo.persistPharmacyItem(pharmacyItem)
-                if inventoryItem is None: return
-                self.createInventoryItem(dict(inventoryItem,pharmacy_item_id=new_pharmacyItem.id))
-                return 
+                self.createPharmacyItem(pharmacyItem)
         except HTTPException as e:
             raise BAD_REQUEST(e.detail)
         return
@@ -193,7 +158,6 @@ class InventoryService:
         try:
             for transactionType in transactionTypes:
                 self.inventory_repo.persistTransactionType(transactionType)
-                return
         except HTTPException as e:
             raise BAD_REQUEST(e.detail)
         return
